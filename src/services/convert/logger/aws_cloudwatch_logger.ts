1bc2a5d77887eaa034ce7901cffe9fe748b96910
VVRPavan <vvr.pavan@wadzpay.com>
2022-09-15 00:14:26 +0530

    fix issues for AWS Cloudwatch logging

--- src/services/convert/logger/aws_cloudwatch_logger.ts
+++ src/services/convert/logger/aws_cloudwatch_logger.ts
@@ -1,35 +1,45 @@
 "use strict";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 import {AWSConfig} from "../conversionethusdt";
 
-var winston = require('winston'),
-    CloudWatchTransport = require('winston-cloudwatch');
+var winston = require('winston');
+    
+var WinstonCloudWatch = require('winston-cloudwatch');
 
 var NODE_ENV = process.env.NODE_ENV || 'dev';
 
-const logger = winston.createLogger({
+var LOG_GROUP_NAME = process.env.LOG_GROUP_NAME || 'wadzpay-usdtbotrestapi'
+console.log('LOG_GROUP_NAME....',LOG_GROUP_NAME);
+
+const logger = new winston.createLogger({
+  format: winston.format.json(),
   transports: [
-    new winston.transports.Console({
-      timestamp: true,
-      colorize: true,
-    })
-  ]
+      new (winston.transports.Console)({
+          timestamp: true,
+          colorize: true,
+      })
+ ]
 });
 
-var config = {
-  logGroupName: process.env.log_group_name,
-  logStreamName: NODE_ENV,
-  createLogGroup: false,
-  createLogStream: true,
-  awsConfig: AWSConfig,
-  formatLog: function (item:any) {
-    return item.level + ': ' + item.message + ' ' + JSON.stringify(item.meta)
-  }
+logger.level = process.env.LOG_LEVEL || "silly";
+
+console.log('log level....',logger.level);
+
+var config = {  
+  logGroupName: process.env.CLOUDWATCH_GROUP_NAME,
+  logStreamName: `${process.env.CLOUDWATCH_GROUP_NAME}-${process.env.NODE_ENV}`,
+  awsAccessKeyId: process.env.AWS_ACCESS_KEY,
+  awsSecretKey: process.env.AWS_SECRET_KEY,
+  awsRegion: process.env.AWS_REGION,
+  messageFormatter: ( level: any, message: any, additionalInfo: any ) =>  `[${level}] : ${message} \nAdditional Info: ${JSON.stringify(additionalInfo)}}`
 }
 
-if (NODE_ENV != 'dev') logger.add(CloudWatchTransport, config);
+if (NODE_ENV != 'dev') logger.add(new WinstonCloudWatch(config));
 
-logger.level = process.env.LOG_LEVEL || "silly";
+
 
 logger.stream = {
   write: function(message:any, encoding:any) {
